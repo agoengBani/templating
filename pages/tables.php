61d1dc674bed2db74da1bd5443ebd957b7c7c1eb
Agung Syabani <agungsyabani99@gmail.com>
2024-04-20 10:28:44 +0700

    update

--- pages/tables.php
+++ pages/tables.php
@@ -99,7 +99,7 @@
                 </g>
               </svg>
             </div>
-            <span class="nav-link-text ms-1">Insert Profile</span>
+            <span class="nav-link-text ms-1">Insert Data</span>
           </a>
         </li>
       </ul>
